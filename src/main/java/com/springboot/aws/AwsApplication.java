48a1ab8533e1cb76128d932f9ebf58686670981c
Jun981020 <flwnsgud@naver.com>
2022-12-28 18:10:36 +0900

    @EnableJpaAuditing 주석 처리

--- src/main/java/com/springboot/aws/AwsApplication.java
+++ src/main/java/com/springboot/aws/AwsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class AwsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AwsApplication.class, args);
